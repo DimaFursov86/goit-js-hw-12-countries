5c2f0a2a151e5d8dc6e7ed70b9ba66f2571dc2c4
Dima <78323304+DimaFursov86@users.noreply.github.com>
2021-08-14 06:00:53 +0300

    ending

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,6 @@
 
 export default function fetchCountries(searchQuery) {
     
-    // const query = searchQuery.target.value;
-   
     return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
         .then((response) => {
             return response.json();
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+
+import countryCardTpl from './templates/countrycard.hbs';
+
 import './sass/main.scss';
 import fetchCountries from './fetchCountries';
 import { error} from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
+
 const refs = {
     input: document.querySelector('.js-input'),
     countriesList: document.querySelector('.js-list-contries')
+    
 }
 
 const debounce = require('lodash.debounce');
@@ -14,26 +19,31 @@ refs.input.addEventListener('input', debounce(handleInput, 500));
 
 function handleInput(e) {
     
-   const searchQuery = e.target.value;
+   const searchQuery = e.target.value.trim();
 
    fetchCountries(searchQuery)
             .then((countries) => {
                 
-                const countriesHtml = countries.map((country) => `<li>${country.name}</li>`)
+                const countriesHtml = countries.map((country) => `<li class="cardList">${country.name}</li>`)
                 
                 if (countriesHtml.length > 10) {
                     error({delay: 1300, width: '310px', text: 'Too many matches found. Please enter more specific query!' });
                     refs.countriesList.innerHTML = ``;
+                    
                 }
                 
                 else if (countriesHtml.length === 1) {
-                    console.log('Ok!')
+                    const markup = countryCardTpl(countries[0])
+                    refs.countriesList.innerHTML = `<li class="noMarker">${markup}</li>`;
                 }
                 else  {
-                     refs.countriesList.innerHTML = countriesHtml;
+                    refs.countriesList.innerHTML = countriesHtml;
+                    
                 };
 
             })
+   
             // .catch(refs.countriesList.innerHTML = `<li class="noMarker"><p>Wrong value</p></li>`);
     
 }
+ 
